e3c8880f0774a4574f57248c180f9ebc163b355b
ThandoLNjili <thandolnjili@gmail.com>
2019-11-21 10:35:07 +0200

    Updates

---
+++ precache-manifest.dc7ef5b3e20cf12fbb64eb01738700f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138f07f8016c1410effd273e39fc8851",
+    "url": "/fictitiouscompany/index.html"
+  },
+  {
+    "revision": "57a76412358436ecfff8",
+    "url": "/fictitiouscompany/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d3016e6f8e417da61eab",
+    "url": "/fictitiouscompany/static/css/main.928f8ea8.chunk.css"
+  },
+  {
+    "revision": "57a76412358436ecfff8",
+    "url": "/fictitiouscompany/static/js/2.3f56525a.chunk.js"
+  },
+  {
+    "revision": "d3016e6f8e417da61eab",
+    "url": "/fictitiouscompany/static/js/main.ad2f7afe.chunk.js"
+  },
+  {
+    "revision": "62c110227706d05ec767",
+    "url": "/fictitiouscompany/static/js/runtime~main.7a47c2d8.js"
+  },
+  {
+    "revision": "ff9d3636d8b1ed7b798567e23c23446c",
+    "url": "/fictitiouscompany/static/media/logo.ff9d3636.png"
+  },
+  {
+    "revision": "af2224102cd497837809b2bb082b3e11",
+    "url": "/fictitiouscompany/static/media/user-icon.af222410.jpg"
+  }
+]);
